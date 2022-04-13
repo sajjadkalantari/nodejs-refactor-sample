f34aec7c6cf39ded323f68c15f43ab1f3f856637
sjdkln <sajadkalantari@outlook.com>
2022-04-13 23:00:53 +0430

    add comments for controller

--- src/controllers/defect-controller.js
+++ src/controllers/defect-controller.js
@@ -3,32 +3,35 @@ const router = express.Router();
 const { DefectInfoService } = require('../services/defect-info-service');
 const defectInfoService = new DefectInfoService();
 
+//get defectinfo by machineid
 router.get("/:machineId", async (req, res) => {
     let { machineId } = req.params;
     let defectInfo = await defectInfoService.getDefectInfo(machineId);
     res.send(defectInfo);
 });
 
+//update defect
 router.put("/", async (req, res) => {
     let { personalNumber, description, machineId } = req.body;
     let result = await defectInfoService.setDefect(personalNumber, description, machineId);
     res.send(result);
 });
 
-
+//get defect status by machineid
 router.get("/status/:machineId", async (req, res) => {
     let { machineId } = req.params;
     let defectInfo = await defectInfoService.getDefectStatus(machineId);
     res.send(defectInfo);
 });
 
-
+//update defect status 
 router.put("/status", async (req, res) => {
     let { machineId, defect_time, status } = req.body;
     let result = await defectInfoService.setDefect(machineId, defect_time, status);
     res.send(result);
 });
 
+//get all defects
 router.get("/", async (req, res) => {    
     let defectInfo = await defectInfoService.getAllDefect();
     res.send(defectInfo);

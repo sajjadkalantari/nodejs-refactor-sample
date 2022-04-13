021ee0bee1e6aad7e0bdaa1f55d22df124802f47
sjdkln <sajadkalantari@outlook.com>
2022-04-13 21:08:55 +0430

    fix routes

--- src/controllers/defect-controller.js
+++ src/controllers/defect-controller.js
@@ -3,33 +3,33 @@ const router = express.Router();
 const { DefectInfoService } = require('../services/defect-info-service');
 const defectInfoService = new DefectInfoService();
 
-router.get("defect/:machineId", async (req, res) => {
+router.get("/:machineId", async (req, res) => {
     let { machineId } = req.params;
     let defectInfo = await defectInfoService.getDefectInfo(machineId);
     res.send(defectInfo);
 });
 
-router.put("defect", async (req, res) => {
+router.put("/", async (req, res) => {
     let { personalNumber, description, machineId } = req.body;
     let result = await defectInfoService.setDefect(personalNumber, description, machineId);
     res.send(result);
 });
 
 
-router.get("defect/status/:machineId", async (req, res) => {
+router.get("/status/:machineId", async (req, res) => {
     let { machineId } = req.params;
     let defectInfo = await defectInfoService.getDefectStatus(machineId);
     res.send(defectInfo);
 });
 
 
-router.put("defect/status", async (req, res) => {
+router.put("/status", async (req, res) => {
     let { machineId, defect_time, status } = req.body;
     let result = await defectInfoService.setDefect(machineId, defect_time, status);
     res.send(result);
 });
 
-router.get("defect", async (req, res) => {    
+router.get("/", async (req, res) => {    
     let defectInfo = await defectInfoService.getAllDefect();
     res.send(defectInfo);
 });
